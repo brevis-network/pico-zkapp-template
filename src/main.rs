b672d584a49a9ee07b7b2bcb3403edef79292909
kaiwei-0 <kaiwei@brevis.network>
2025-01-12 14:20:59 +0800

    Update zkvm io api

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![no_main]
 
 pico_sdk::entrypoint!(main);
-use pico_sdk::io::read;
+use pico_sdk::io::read_as;
 use pico_sdk::io::commit;
 
 pub fn main() {
-    let n: i32 = read();
+    let n: i32 = read_as();
     let mut a: u64 = 0;
     let mut b: u64 = 1;
     for _ in 0..n {
